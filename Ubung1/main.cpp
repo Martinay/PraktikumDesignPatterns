b0dc6c421ddcc96c06b4ac4640e56d3e80541be2
MartinAy <martinay@outlook.com>
2016-10-20 11:41:36 +0200

    added build script

---
+++ Ubung1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World";
+   int a;
+   cin >> a;
+   return 0;
+}
---
+++ build.sh
@@ -0,0 +1,3 @@
+mkdir -p out
+g++ -g -o ./out/main1 ./Ubung1/main.cpp
+echo "build successful"
