8e0ff8025a592235c7962bc211e934d97a0789cb
MartinAy <martinay@outlook.com>
2016-11-04 10:56:20 +0100

    build scripts angepasst

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 mkdir -p out
-g++ -g -o ./out/main1 ./Ubung1/Main.cpp ./Ubung1/Term.cpp ./Ubung1/Add.cpp ./Ubung1/Sub.cpp ./Ubung1/Mul.cpp ./Ubung1/Variable.cpp
-echo "build script finished"
+./Ubung1/build.sh
+echo "build script Ubung 1 finished"
+
+./Ubung2/build.sh
+echo "build script Ubung 2 finished"
