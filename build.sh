0d5d7f6954993ccf6f7ac3445cdb7605335a6670
Martinay <martinay@outlook.com>
2017-01-20 12:08:10 +0100

    run all builds when run build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 mkdir -p out
-#./Ubung1/build.sh
-#echo "build Ubung 1 finished"
+./Ubung1/build.sh
+echo "build Ubung 1 finished"
 
-#./Ubung2/build.sh
-#echo "build Ubung 2 finished"
+./Ubung2/build.sh
+echo "build Ubung 2 finished"
 
-# ./Ubung3/build.sh
-# echo "build Ubung 3 finished"
+./Ubung3/build.sh
+echo "build Ubung 3 finished"
 
-#./Ubung4/build.sh
-#echo "build Ubung 4 finished"
+./Ubung4/build.sh
+echo "build Ubung 4 finished"
 
-#./Ubung5/build.sh
-#echo "build Ubung 5 finished"
+./Ubung5/build.sh
+echo "build Ubung 5 finished"
 
 ./Ubung6/build.sh
 echo "build Ubung 6 finished"
 
-#./Ubung7/build.sh
-#echo "build Ubung 7 finished"
+./Ubung7/build.sh
+echo "build Ubung 7 finished"
